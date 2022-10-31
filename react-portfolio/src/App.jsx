35fd3136cb3be960dc21c6ee83d6ae8f8fa9122a
Bunyameen <bunyameen@gmail.com>
2022-10-31 23:28:53 +0100

    working on the Navbar

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import './components/styles.css'
+import { Nav } from './components/Nav'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div className="container">
       <h1>PortFolio</h1>
+      <Nav />
       </div>
   )
 }
---
+++ react-portfolio/src/components/Nav.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './styles.css'
+
+export const Nav = () => {
+  return (
+    <nav className='container'>
+        <h1>Bunyameen Shola</h1>
+        <ul>
+            <a href="">
+                <li>About</li>
+            </a>
+            <a href="">
+                <li>Work</li>
+            </a>
+            <a href="">
+                <li>Contact</li>
+            </a>
+            
+        </ul>
+        </nav>
+  )
+}
